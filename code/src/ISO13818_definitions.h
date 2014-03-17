c7e50816808f07ab659b7b79bb10ec316b3c8e4d
Lucas <nasall.2@gmail.com>
2014-03-16 23:20:30 -0300

    new file:   ISO13818_definitions.h 	modified:   a.out 	new file:   audio_ES.ES 	modified:   main.c 	new file:   video_ES.ES

---
+++ code/src/ISO13818_definitions.h
@@ -0,0 +1,8 @@
+/*
+	This file defines for the code the mandatory aspects of ISO13818 standard.
+
+
+			Lucas
+*/
+
+#define SYNC_BYTE 0x47
--- code/src/main.c
+++ code/src/main.c
@@ -6,17 +6,35 @@
 void main(){
 
 	TS_packet_header header;
+	FILE * video_ES_ptr;
+	FILE * audio_ES_ptr;
+	FILE * output_TS_ptr;
+	int i;
 
 	printf("Hello world!\n");
 
-	header.sync_byte = 0x00;
+//	header.sync_byte = 0x00;
 	header.PID = 0x0555; // 0 0000 0000 0000
 	header.adaptation_field_control = 0x3;
 	header.continuity_counter = 0xF;
 	
-	printf("HEADER sync byte:\n%x\n",header.sync_byte);
-	printf("HEADER PID:\n%x\n",header.PID);
-	printf("HEADER adaptation_field_control:\n%x\n",header.adaptation_field_control);
-	printf("HEADER continuity counter:\n%x\n",header.continuity_counter);
+//	printf("HEADER sync byte:\n%x\n",header.sync_byte);
+//	printf("HEADER PID:\n%x\n",header.PID);
+//	printf("HEADER adaptation_field_control:\n%x\n",header.adaptation_field_control);
+//	printf("HEADER continuity counter:\n%x\n",header.continuity_counter);
+
+	video_ES_ptr = fopen("video_ES.ES","w");
+	audio_ES_ptr = fopen("audio_ES.ES","w");
+
+//	for(i = 0; i<=255; i++) {
+//		putc(0x00,video_ES_ptr);
+		//printf("%x \t",i);
+//	}
+
+//	for(i = 0; i<=255; i++) {
+//		putc(0xff,audio_ES_ptr);
+		//printf("%x \t",i);
+//	}
 
 }
+
